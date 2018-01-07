97417bc44bf5fea23d5054fb46ab41adbbc476d0
Matt D <matt3dixon@gmail.com>
2018-01-07 12:04:46 +1000

    cleaned up mix script such that it actually works

--- mix.sh
+++ mix.sh
@@ -1,1 +1,17 @@
-ffmpeg -i 80YearsOfHappy.mp4 -i Yonkers-TylerTheCreator.mp4 -strict -2 -filter_complex "[1:a][1:a]amerge=inputs=2[aud]; [0:v]concat=n=1:v=1:a=0[vid:v]" -map [aud] -map [vid:v] -shortest ./out.mp4
+if (( $# < 3 )); then
+    echo "mix.sh videoin audioin output"
+    exit 1
+fi
+
+set -o history -o histexpand
+
+ffmpeg \
+    -an -i ${1} \
+    -vn -i ${2} \
+    -strict -2 -filter_complex "[1:a][1:a]amerge=inputs=2[a] ; [0:v]concat=n=1:v=1:a=0[vid:v]" -map [a] -map [vid:v] -shortest ${3}
+
+echo !!
+
+# ffmpeg -i input.mov -filter_complex "[0:v]scale=-2:720,format=yuv420p[v];[0:a]amerge=inputs=$(
+#     ffprobe -loglevel error -select_streams a -show_entries stream=codec_type -of csv=p=0 input.mov | wc -l
+#     )[a]" -map "[v]" -map "[a]" -c:v libx264 -crf 23 -preset medium -c:a libmp3lame -ar 44100 -ac 2 output.mov
